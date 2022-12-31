ea976bd97457e2ca09088fe3392b12164517ceb2
Armando <armando_avila_4>
2022-12-31 03:11:33 -0600

    Practica 1 Iconos y LaunchScreen

---
+++ practica1_calculadora/practica1_calculadora/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  practica1_calculadora
+//
+//  Created by Jorge Armando Avila Estrada on 31/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
